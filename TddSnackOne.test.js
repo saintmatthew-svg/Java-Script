c5cd9f17caf37faf09d434e2653a95561ad4c6be
saintmatthew-svg <onyekachimatthew00@gmail.com>
2025-04-02 14:34:41 +0100

    commited

--- TddSnackOne.test.js
+++ TddSnackOne.test.js
@@ -2,8 +2,7 @@ const {AirConditioner, TurnOn, TurnOff, Increase, Decrease} = require("./TddSnac
 
 describe("default", () => {
     beforeEach(() => {
-        minTemperature = 16;
-        maxTemperature = 30;
+        Temperature = 16;
     });
 
     test("Ac is on", () => {
@@ -15,19 +14,27 @@ describe("default", () => {
     });
 
     test("Ac can increase above 16", () => {   
-        expect(Increase(minTemperature)).toBe(17);
+        expect(Increase(Temperature)).toBe(17);
     });
 
     test("Ac can decrease below 30", () => {
-        expect(Decrease(maxTemperature)).toBe(29)
+        let Temperature = 30;
+        expect(Decrease(Temperature)).toBe(29)
     });
 
     test("Ac does not go below 16", () => {
-        expect(Decrease(minTemperature)).toBe(16);
+        Decrease(Temperature);
+        expect(Decrease(Temperature)).toBe(16);
+        Decrease(Temperature);
+        expect(Decrease(Temperature)).toBe(16);
     });
 
     test("Ac does not go above 30", () => {
-        expect(Increase(maxTemperature)).toBe(30);
+        let Temperature = 30;
+        Increase(Temperature);
+        expect(Increase(Temperature)).toBe(30);
+        Increase(Temperature);
+        expect(Increase(Temperature)).toBe(30);
     });
 
 });
